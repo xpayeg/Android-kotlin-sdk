ead30b949d70314774fd65bdb046c4577b12de82
Islam Rostom <islam.rostom93@gmail.com>
2020-11-04 12:29:22 +0200

    fix server error tests

--- app/src/main/java/com/xpay/kotlinutils/models/api/Status.kt
+++ app/src/main/java/com/xpay/kotlinutils/models/api/Status.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class Status (
     @SerializedName("code") val code : Int,
     @SerializedName("message") val message : String,
-    @SerializedName("errors") val errors : List<String>
+    @SerializedName("errors") val errors : List<Any>
 )
--- app/src/main/java/com/xpay/kotlinutils/models/api/prepare/PrepareAmountData.kt
+++ app/src/main/java/com/xpay/kotlinutils/models/api/prepare/PrepareAmountData.kt
@@ -3,8 +3,8 @@ package com.xpay.kotlinutils.models.api.prepare
 import com.google.gson.annotations.SerializedName
 
 data class PrepareAmountData (
-    @SerializedName("total_amount") val total_amount : Double,
-    @SerializedName("total_amount_currency") val total_amount_currency : String,
-    @SerializedName("CASH") val CASH : TotalAmount,
-    @SerializedName("KIOSK") val KIOSK : TotalAmount
+    @SerializedName("total_amount") val total_amount : Double?,
+    @SerializedName("total_amount_currency") val total_amount_currency : String?,
+    @SerializedName("CASH") val CASH : TotalAmount?,
+    @SerializedName("KIOSK") val KIOSK : TotalAmount?
 )
